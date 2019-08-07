3612db6ddab999b161282c8d4d6279365c9df6c1
good--afternoon <goodafternooooooon@gmail.com>
2019-08-07 17:39:25 +0900

    webpack+Babel+jQueryの構成に変更

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+
+module.exports = {
+  // モード値を production に設定すると最適化された状態で、
+  // development に設定するとソースマップ有効でJSファイルが出力される
+  mode: "development",
+
+  // メインとなるJavaScriptファイル（エントリーポイント）
+  entry: "./src/js-es2019/app.js",
+
+  output: {
+    // ファイルの出力先
+    path: path.resolve(__dirname, 'docs/assets/js'),
+    filename: "bundle.js"
+  },
+
+  module: {
+    rules: [
+      {
+        // 拡張子 .js の場合
+        test: /\.js$/,
+        use: [
+          {
+            // Babel を利用する
+            loader: 'babel-loader',
+            // Babel のオプションを指定する
+            options: {
+              presets: [
+                // プリセットを指定することで、ES2019 を ES5 に変換
+                "@babel/preset-env"
+              ]
+            }
+          }
+        ]
+      }
+    ]
+  }
+}

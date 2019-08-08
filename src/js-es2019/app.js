166fb9db53f12f6d1647cac4b2275b1f06936954
good--afternoon <goodafternooooooon@gmail.com>
2019-08-08 22:06:12 +0900

    脱jQuery

--- src/js-es2019/app.js
+++ src/js-es2019/app.js
@@ -1,13 +1,9 @@
-import jQuery from 'jquery';
-
-// ドルマークに参照を代入(慣習的な $ を使うため)
-const $ = jQuery;
-
 /**
- * マウスポインタの座標を取得
- */    
-$(window).mousemove(function(){
-  var x = event.clientX,
-      y = event.clientY;
-  $('#js-mouse').css({'top': y + 20, 'left': x + 20 });
+ * マウス移動時の座標を出力
+ */   
+const target = document.querySelector('#js-images');
+const mouse = document.querySelector('#js-mouse');
+target.addEventListener('mousemove', (event) => {
+  mouse.style.top = event.clientY + 20 + 'px';
+  mouse.style.left = event.clientX + 20 + 'px';
 });

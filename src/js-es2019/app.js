7537c602f5f4684a33ca77c7313a01b76793877f
good--afternoon <goodafternooooooon@gmail.com>
2019-08-17 13:56:46 +0900

    touchmoveを実装

--- src/js-es2019/app.js
+++ src/js-es2019/app.js
@@ -1,9 +1,31 @@
+import _ from 'lodash';
+
 /**
  * マウス移動時の座標を出力
- */   
-const target = document.querySelector('#js-images');
+ */
+const target = document.querySelector('#js-wrapper');
 const mouse = document.querySelector('#js-mouse');
-target.addEventListener('mousemove', (event) => {
-  mouse.style.top = event.clientY + 20 + 'px';
-  mouse.style.left = event.clientX + 20 + 'px';
-});
+
+// 座標の初期値
+mouse.style.top = 'calc(50% - 100px)';
+mouse.style.left = 'calc(50% - 100px)';
+
+// mouse event
+target.addEventListener('mousemove', _.throttle((event) => {
+  mouse.style.top = event.clientY - 100 + 'px';
+  mouse.style.left = event.clientX - 100 + 'px';
+}, 50)); //実行を間引く
+
+// touch event
+target.addEventListener('touchstart', _.throttle(event => TouchEventFunc(event), 20));
+
+target.addEventListener('touchmove', _.throttle(event => TouchEventFunc(event), 20));
+
+function TouchEventFunc(event) {
+  event.preventDefault(); //スクロールを無効
+
+  const touch = event.changedTouches;
+  mouse.style.top = touch[0].pageY.toFixed(2) - 100 + 'px';
+  mouse.style.left = touch[0].pageX.toFixed(2) - 100 + 'px';
+  passive: false;
+}
